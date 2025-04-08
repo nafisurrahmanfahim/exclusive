c32be995681ce463a04f9d16e63320515e3bc4a1
nafisurrahmanfahim <nafisurr93@gmail.com>
2025-04-08 12:56:42 +0600

    demo

--- src/components/checkOute.js
+++ src/components/checkOute.js
@@ -1,27 +1,27 @@
-// import { createSlice } from '@reduxjs/toolkit'
-// import { act } from 'react'
+import { createSlice } from '@reduxjs/toolkit'
+import { act } from 'react'
 
-// const initialState = {
-//   sellItem: localStorage.getItem('sell') ? JSON.parse(localStorage.getItem('sell')):[],
-// }
+const initialState = {
+  sellItem: localStorage.getItem('sell') ? JSON.parse(localStorage.getItem('sell')):[],
+}
 
-// export const checkOutSlice = createSlice({
-//   name: 'counter',
-//   initialState,
-//   reducers: {
-//     checkOut: (state, action) => {
-//         let checkOutItem = state.sellItem.findIndex((item)=> item.id == action.payload.id)
-//         if(checkOutItem !== -1) {
-//             state.sellItem[checkOutItem].qun +=1
-//         } else{
-//             state.sellItem = [...state.sellItem, action.payload]
-//             localStorage.setItem('sell', JSON.stringify(state.sellItem))
-//         }
-//     }
-//   },
-// })
+export const checkOutSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    checkOut: (state, action) => {
+        let checkOutItem = state.sellItem.findIndex((item)=> item.id == action.payload.id)
+        if(checkOutItem !== -1) {
+            state.sellItem[checkOutItem].qun +=1
+        } else{
+            state.sellItem = [...state.sellItem, action.payload]
+            localStorage.setItem('sell', JSON.stringify(state.sellItem))
+        }
+    }
+  },
+})
 
-// // Action creators are generated for each case reducer function
-// export const { checkOut } = checkOutSlice.actions
+// Action creators are generated for each case reducer function
+export const { checkOut } = checkOutSlice.actions
 
-// export default checkOutSlice.reducer
+export default checkOutSlice.reducer
